11c3f713181607247231bc472cd8721606142711
Dimitar Gramenov <goshoburkana123@gmail.com>
2022-09-01 21:19:28 +0300

    Jump Animation

--- NewBeginning/Assets/Scripts/PlayerMovements.cs
+++ NewBeginning/Assets/Scripts/PlayerMovements.cs
@@ -4,51 +4,48 @@ using UnityEngine;
 
 public class PlayerMovements : MonoBehaviour
 {
-    [SerializeField]private float speed;
-    [SerializeField]private LayerMask platformLayerMask;
-
+    
+    [SerializeField] private float speed;
     private Rigidbody2D body;
-    private BoxCollider2D boxCollider2D;
     private Animator anim;
     private bool grounded;
 
-
     private void Awake()
     {
+        //Grabs references for rigidbody and animator from game object.
         body = GetComponent<Rigidbody2D>();
-        boxCollider2D = transform.GetComponent<BoxCollider2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        body.velocity = new Vector2(speed, body.velocity.y) ;
 
-        if (Input.GetKey(KeyCode.Space) && IsGrounded())
-        {
-            body.velocity = new Vector2(body.velocity.x, speed);
-            anim.SetTrigger("jump");
-        }
-            
+        body.velocity = new Vector2(speed, body.velocity.y);
+
+
+
+        if (Input.GetKey(KeyCode.Space) && grounded)
+            Jump();
+
+        //sets animation parameters
+
+        anim.SetBool("grounded", grounded);
     }
 
-    private bool IsGrounded() {
-        float extraHeight = 0.5f;
-        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down, extraHeight, platformLayerMask);
-        Color rayColor;
-        if (raycastHit.collider != null)
-        {
-            rayColor = Color.green;
-        }else
-        {
-            rayColor = Color.red;
-        }
-        Debug.DrawRay(boxCollider2D.bounds.center + new Vector3(boxCollider2D.bounds.extents.x, 0), Vector2.down * (boxCollider2D.bounds.extents.y + extraHeight), rayColor);
-        Debug.DrawRay(boxCollider2D.bounds.center - new Vector3(boxCollider2D.bounds.extents.x, 0), Vector2.down * (boxCollider2D.bounds.extents.y + extraHeight), rayColor);
-        Debug.DrawRay(boxCollider2D.bounds.center - new Vector3(boxCollider2D.bounds.extents.x, boxCollider2D.bounds.extents.y), Vector2.right * (boxCollider2D.bounds.extents.y + extraHeight), rayColor);
-        return raycastHit.collider != null;
-    }   
-        
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
+    }
+
+
 
 }
 //
- 
