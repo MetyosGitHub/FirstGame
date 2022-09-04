344db07c18647e572cb6404f4a146845025edff7
Dimitar Gramenov <goshoburkana123@gmail.com>
2022-09-04 21:40:57 +0300

    Slide

--- NewBeginning/Assets/Scripts/PlayerMovements.cs
+++ NewBeginning/Assets/Scripts/PlayerMovements.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class PlayerMovements : MonoBehaviour
 {
-    
+
     [SerializeField] private float speed;
+    private BoxCollider2D boxCollider;
     private Rigidbody2D body;
     private Animator anim;
     private bool grounded;
+    private bool slideAuthority;
 
     private void Awake()
     {
         //Grabs references for rigidbody and animator from game object.
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
+
     }
 
     private void Update()
@@ -26,7 +30,10 @@ public class PlayerMovements : MonoBehaviour
 
         if (Input.GetKey(KeyCode.Space) && grounded)
             Jump();
-
+        if (Input.GetKey(KeyCode.LeftShift) && grounded && slideAuthority)
+        {
+            Slide();
+        }
         //sets animation parameters
 
         anim.SetBool("grounded", grounded);
@@ -39,13 +46,37 @@ public class PlayerMovements : MonoBehaviour
         grounded = false;
     }
 
+    private void Slide()
+    {
+        slideAuthority = false;
+
+        anim.SetTrigger("slide");
+        body.velocity = new Vector2(speed * 0.8f, body.velocity.y);
+        boxCollider.enabled = false;
+        StartCoroutine(stopSlide());
+
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Ground")
+        {
             grounded = true;
+            slideAuthority = true;
+        }
+
+
     }
 
+    IEnumerator stopSlide()
+    {
+        yield return new WaitForSeconds(0.75f);
+        boxCollider.enabled = true;
+        yield break;
+
+        
 
 
+    }
 }
 //
