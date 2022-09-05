b1d8a2bd100976b6cf24127f8091361f2228c9e7
Goskoo <dogmanjp31@gmail.com>
2022-09-06 00:24:29 +0300

    Respawn & Assets

---
+++ NewBeginning/Assets/Scripts/Death.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform respawnPoint;
+
+     void OnTriggerEnter2D(Collider2D other)
+    {
+        player.transform.position = respawnPoint.transform.position; 
+    }
+
+
+}
